cdd6b09ac3b5ae45a4c8ace9abdc816219324fa0
arip1806 <35683377+arip1806@users.noreply.github.com>
2024-10-28 23:48:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,18 @@ st.header("Genetic Algorithm", divider="gray")
 
 import random
 
+#POP_SIZE: Number of Chromosomes in our list.
+POP_SIZE = 500
+
+#MUT_RATE: Rate at which our string will be changed.
+MUT_RATE = 0.2
+
+#TARGET: Our goal.
+TARGET = 'arif'
+
+#GENES: Options from which our population would be created.
+GENES = ' abcdefghijklmnopqrstuvwxyz'
+
 #initialization
 
 def initialize_pop(TARGET):
