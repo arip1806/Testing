306c31cea8dd099632377a47562c622af52157df
arip1806 <35683377+arip1806@users.noreply.github.com>
2024-10-29 01:43:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,7 +121,7 @@ def main(POP_SIZE, MUT_RATE, TARGET, GENES):
         break
         st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
       generation+=1
-      if calculate_button:
+     if calculate_button:
     TARGET = target_input.upper()
     MUT_RATE = mutation_rate_input
 result = main(POP_SIZE, MUT_RATE, TARGET, GENES)
