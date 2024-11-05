dfd0b74a7e6e7acb2a377b7a7cb28ae12b32e9d3
arip1806 <35683377+arip1806@users.noreply.github.com>
2024-11-05 08:50:48 +0800

    Update search.py

--- pages/search.py
+++ pages/search.py
@@ -18,22 +18,27 @@ for i in range(num_cities):
     y_coord = float(input(f"Enter the y-coordinate for {city_name}: "))
     city_names.append(city_name)
     city_coords.append((x_coord, y_coord))
-fig, ax = plt.subplots()
+n_population = 250
+crossover_per = 0.8
+mutation_per = 0.2
+n_generations = 200
 
-ax.grid(False)  # Grid
+# Pastel Pallete
+colors = sns.color_palette("pastel", len(cities_names))
 
-for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
-    color = colors[i]
-    icon = city_icons[city]
-    ax.scatter(city_x, city_y, c=[color], s=1200, zorder=2)
-    ax.annotate(icon, (city_x, city_y), fontsize=40, ha='center', va='center', zorder=3)
-    ax.annotate(city, (city_x, city_y), fontsize=12, ha='center', va='bottom', xytext=(0, -30),
-                textcoords='offset points')
-
-    # Connect cities with opaque lines
-    for j, (other_city, (other_x, other_y)) in enumerate(city_coords.items()):
-        if i != j:
-            ax.plot([city_x, other_x], [city_y, other_y], color='gray', linestyle='-', linewidth=1, alpha=0.1)
+# City Icons
+city_icons = {
+    "Gliwice": "♕",
+    "Cairo": "♖",
+    "Rome": "♗",
+    "Krakow": "♘",
+    "Paris": "♙",
+    "Alexandria": "♔",
+    "Berlin": "♚",
+    "Tokyo": "♛",
+    "Rio": "♜",
+    "Budapest": "♝"
+}
 
 fig.set_size_inches(16, 12)
 st.pyplot(fig)
