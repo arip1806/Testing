453d22746733952c01c7db81c3f87fed51f6c16a
arip1806 <35683377+arip1806@users.noreply.github.com>
2024-11-06 20:37:26 +0800

    Update search.py

--- pages/search.py
+++ pages/search.py
@@ -8,27 +8,34 @@ import pandas as pd
 import seaborn as sns
 import streamlit as st
 
-def get_user_input():
-    num_cities = st.number_input("Enter the number of cities:", min_value=2, value=5)
-    cities = []
-    coords = []
-    for i in range(num_cities):
-        city_name = st.text_input(f"Enter the name of city {i+1}:")
-        x_coord = st.number_input(f"Enter the x-coordinate for {city_name}:")
-        y_coord = st.number_input(f"Enter the y-coordinate for {city_name}:")
-        cities.append(city_name)
-        coords.append((x_coord, y_coord))
-    return cities, coords
 
-def visualize_cities(cities, coords):
-    fig, ax = plt.subplots()
-    for i, (city, (x, y)) in enumerate(zip(cities, coords)):
-        ax.scatter(x, y, color='blue', marker='o')
-        ax.annotate(city, (x, y), fontsize=10)
-    plt.xlabel("X-coordinate")
-    plt.ylabel("Y-coordinate")
-    plt.title("City Locations")
-    st.pyplot(fig)
+x = [0,3,6,7,15,10,16,5,8,1.5]
+y = [1,2,1,4.5,-1,2.5,11,6,9,12]
+cities_names = ["Gliwice", "Cairo", "Rome", "Krakow", "Paris", "Alexandria", "Berlin", "Tokyo", "Rio", "Budapest"]
+city_coords = dict(zip(cities_names, zip(x, y)))
+n_population = 250
+crossover_per = 0.8
+mutation_per = 0.2
+n_generations = 200
+
+# Pastel Pallete
+colors = sns.color_palette("pastel", len(cities_names))
+
+# City Icons
+city_icons = {
+   "Gliwice": "♕",
+    "Cairo": "♖",
+    "Rome": "♗",
+    "Krakow": "♘",
+    "Paris": "♙",
+    "Alexandria": "♔",
+    "Berlin": "♚",
+    "Tokyo": "♛",
+    "Rio": "♜",
+    "Budapest": "♝"
+}
+
+fig, ax = plt.subplots()
 
 #population
 def initial_population(cities_list, n_population = 250):
