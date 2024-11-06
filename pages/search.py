2d799193d8bad00b3a2320302a94cf80c0a283bb
arip1806 <35683377+arip1806@users.noreply.github.com>
2024-11-06 20:41:55 +0800

    Update search.py

--- pages/search.py
+++ pages/search.py
@@ -9,9 +9,9 @@ import seaborn as sns
 import streamlit as st
 
 
-x = [0,3,6,7,15,10,16,5,8,1.5]
-y = [1,2,1,4.5,-1,2.5,11,6,9,12]
-cities_names = ["Gliwice", "Cairo", "Rome", "Krakow", "Paris", "Alexandria", "Berlin", "Tokyo", "Rio", "Budapest"]
+x = input("Enter the coordinate x")
+y = input("Enter the coordinate y")
+cities_names =input("Enter the city name:")
 city_coords = dict(zip(cities_names, zip(x, y)))
 n_population = 250
 crossover_per = 0.8
