572fa713095df6f79aebf87cbda100f83e2465ba
arip1806 <35683377+arip1806@users.noreply.github.com>
2024-11-20 22:44:36 +0800

    Update lamba.py

--- pages/lamba.py
+++ pages/lamba.py
@@ -6,35 +6,34 @@ def rgb_fft(image):
         rgb_fft = np.fft.fftshift(np.fft.fft2((image[:, :, i])))
         fft_images.append(rgb_fft)
         fft_images_log.append(np.log(abs(rgb_fft)))
-    
+
     return fft_images, fft_images_log
-    
-def apply_mask(input_image, mask): 
+def apply_mask(input_image, mask):
     _, mask_thresh = cv2.threshold(mask, 120, 255, cv2.THRESH_BINARY)
     mask_bool = mask_thresh.astype('bool')
     input_image[mask_bool] = 1
-    
-    return input_image 
 
-def apply_mask_all(list_images, list_mask): 
+    return input_image
+
+
+def apply_mask_all(list_images, list_mask):
     final_result = []
-    
+
     for (i,mask) in zip(list_images, list_mask):
         result = apply_mask(i,mask)
         final_result.append(result)
     return final_result
-    
-def create_canvas_draw_instance(background_image, key, height, width): 
+def create_canvas_draw_instance(background_image, key, height, width):
 
     canvas_result = st_canvas(
-        fill_color="rgba(255, 165, 0, 0)",  
+        fill_color="rgba(255, 165, 0, 0)",
         stroke_width=stroke_width,
         stroke_color=stroke_color,
         background_color=bg_color,
         background_image=Image.open(background_image),
         update_streamlit=realtime_update,
         drawing_mode=drawing_mode,
-        height = height, 
+        height = height,
         width = width,
         key=key,
     )
