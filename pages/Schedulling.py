ff6c7d137f0f5f08fc4443eaece48202d8a1b4a6
arip1806 <35683377+arip1806@users.noreply.github.com>
2024-12-13 03:45:41 +0800

    Update Schedulling.py

--- pages/Schedulling.py
+++ pages/Schedulling.py
@@ -39,8 +39,8 @@ GEN = 100
 POP = 50
 #CO_R = 0.8
 #MUT_R = 0.2
-CO_R = st.numberinput('Crossover Rate', min_value=0.0, max_value=0.95, value=0.8, step=0.01)
-MUT_R = st.numberinput('Mutation Rate', min_value=0.01, max_value=0.05, value=0.02, step=0.01)
+CO_R = st.number_input('Crossover Rate', min_value=0.0, max_value=0.95, value=0.8, step=0.01)
+MUT_R = st.number_input('Mutation Rate', min_value=0.01, max_value=0.05, value=0.02, step=0.01)
 EL_S = 2
 
 all_programs = list(ratings.keys()) # all programs
